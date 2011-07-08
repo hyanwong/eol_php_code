2c3d41b6664e43a99e639d06aaaf052c5c5ece2e
Lee Wood <lwood@eol.org>
2011-07-08 16:45:47 -0400

    working - just need to check comments

--- rake_tasks/archive_script.php
+++ rake_tasks/archive_script.php
@@ -1,4 +1,5 @@
 <?php
+echo "\n\n ENTERING: archive_script.php\n";
 
 ////
 // cd $PHP_CODE/rake_tasks
@@ -25,23 +26,25 @@
 //////////////////////////////////////////////////////////////////////////////
 
 
-
+/***/
 ////// ArchiverHierarchyEntries
-include 'ArchiverHierarchyEntries.php';
 
-echo "\n\n LAUNCHING: ArchiverHierarchyEntries\n";
+echo "\n\n REQUIRING: ArchiverHierarchyEntries.php\n";
+require_once 'ArchiverHierarchyEntries.php';
 
+
+echo "\n\n LAUNCHING: my_ArchiverHierarchyEntries\n";
 $my_ArchiverHierarchyEntries = new ArchiverHierarchyEntries();
-#$my_ArchiverHierarchyEntries->display_target_tables();
-#$my_ArchiverHierarchyEntries->get_database_connection_info();
+$my_ArchiverHierarchyEntries->get_database_connection_info();
+$my_ArchiverHierarchyEntries->display_table_names_arr();
 
 #$my_ArchiverHierarchyEntries->note_hierarchy_entries_rowcount_before();
 
 
 
-echo "\n\n LEAVING: ArchiverHierarchyEntries\n";
+echo "\n\n LEAVING: archive_script.php\n";
 exit;
-
+/***/
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -50,23 +53,21 @@ exit;
 
 ////// ArchiverDataObjects
 
-include 'ArchiverDataObjects.php';
+echo "\n\n REQUIRING: ArchiverDataObjects.php\n";
+require_once 'ArchiverDataObjects.php';
 
-echo "\n\n LAUNCHING: ArchiverDataObjects\n";
-
-
+echo "\n\n LAUNCHING: my_ArchiverDataObjects\n";
 $my_ArchiverDataObjects = new ArchiverDataObjects();
 $my_ArchiverDataObjects->get_database_connection_info();
+$my_ArchiverDataObjects->display_table_names_arr();
+
 #$my_ArchiverDataObjects->process_archivable_ids(20);
-
-
 #$my_ArchiverDataObjects->get_database_connection_info();
 #$my_ArchiverDataObjects->process_archivable_ids(3, FALSE);
 #$my_ArchiverDataObjects->display_table_arr();
-#$my_ArchiverDataObjects->display_table_names_arr();
 
 
-echo "\n\n LEAVING: ArchiverDataObjects\n";
+echo "\n\n LEAVING: archive_script.php\n";
 exit;
 
 
